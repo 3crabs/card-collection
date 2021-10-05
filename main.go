01755d6d488a0ccba563de8e5b94aff86f8c6a1c
FedorovVladimir <vladimir.fodorow@gmail.com>
2021-10-05 23:19:14 +0700

    add server with required /info route

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+//AppInfo информация о пиложении
+type AppInfo struct {
+	AppName    string `json:"app_name"`
+	AppVersion string `json:"app_version"`
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	e := echo.New()
+
+	// обязательный роут с информацией о приложении
+	e.GET("/info", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, AppInfo{
+			AppName:    "card-collection",
+			AppVersion: "0.1.0",
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
