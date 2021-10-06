305249a013c05d8d61cf7ebd6949c158afb8ffef
FedorovVladimir <vladimir.fodorow@gmail.com>
2021-10-06 18:36:25 +0700

    clean code

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ type AppInfo struct {
 
 //Card карта - единица коллекционирования
 type Card struct {
+	Id   string `json:"id"`
 	Name string `json:"name"`
 }
 
@@ -41,24 +42,25 @@ func main() {
 		tempCards = make(map[string]Card)
 
 		for _, c := range cards {
-			key := uuid.New().String()
-			m[key] = c
-			tempCards[key] = c
+			id := uuid.New().String()
+			c.Id = id
+			m[id] = c
+			tempCards[id] = c
 		}
 		return c.JSON(http.StatusOK, tempCards)
 	})
 
-	//получаем все карты
+	// получаем все карты
 	e.GET("/cards", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, m)
 	})
 
-	//получаем карту по uuid
-	e.GET("/cards/:key", func(c echo.Context) error {
-		key := c.Param("key")
+	// получаем карту по uuid
+	e.GET("/cards/:id", func(c echo.Context) error {
+		key := c.Param("id")
 		card, ok := m[key]
 		if !ok {
-			return c.JSON(http.StatusNotFound, "Card not found")
+			return c.JSON(http.StatusNotFound, "card not found")
 		}
 		return c.JSON(http.StatusOK, card)
 	})
